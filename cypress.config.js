57a336d2023c45c04277616613b4ef8c7ebbb31e
estrada9166 <estrada9166@hotmail.com>
2022-01-20 22:43:22 -0500

    Upload artifacts

--- cypress.config.js
+++ cypress.config.js
@@ -1,98 +1,99 @@
-const path = require("path");
-const _ = require("lodash");
-const axios = require("axios");
-const dotenv = require("dotenv");
-const Promise = require("bluebird");
-const { percyHealthCheck } = require("@percy/cypress/task");
-const codeCoverageTask = require("@cypress/code-coverage/task");
-const { devServer } = require("@cypress/react/plugins/react-scripts");
+module.exports = {};
+// const path = require("path");
+// const _ = require("lodash");
+// const axios = require("axios");
+// const dotenv = require("dotenv");
+// const Promise = require("bluebird");
+// const { percyHealthCheck } = require("@percy/cypress/task");
+// const codeCoverageTask = require("@cypress/code-coverage/task");
+// const { devServer } = require("@cypress/react/plugins/react-scripts");
 
-dotenv.config({ path: ".env.local" });
-dotenv.config();
+// dotenv.config({ path: ".env.local" });
+// dotenv.config();
 
-const awsConfig = require(path.join(__dirname, "./aws-exports-es5.js"));
+// const awsConfig = require(path.join(__dirname, "./aws-exports-es5.js"));
 
-module.exports = {
-  baseUrl: "http://localhost:3000",
-  projectId: "7s5okt",
-  integrationFolder: "cypress/tests",
-  env: {
-    apiUrl: "http://localhost:3001",
-    mobileViewportWidthBreakpoint: 414,
-    coverage: false,
-    codeCoverage: {
-      url: "http://localhost:3001/__coverage__",
-    },
-    defaultPassword: process.env.SEED_DEFAULT_USER_PASSWORD,
-    paginationPageSize: process.env.PAGINATION_PAGE_SIZE,
+// module.exports = {
+//   baseUrl: "http://localhost:3000",
+//   projectId: "7s5okt",
+//   integrationFolder: "cypress/tests",
+//   env: {
+//     apiUrl: "http://localhost:3001",
+//     mobileViewportWidthBreakpoint: 414,
+//     coverage: false,
+//     codeCoverage: {
+//       url: "http://localhost:3001/__coverage__",
+//     },
+//     defaultPassword: process.env.SEED_DEFAULT_USER_PASSWORD,
+//     paginationPageSize: process.env.PAGINATION_PAGE_SIZE,
 
-    // Auth0
-    auth0_username: process.env.AUTH0_USERNAME,
-    auth0_password: process.env.AUTH0_PASSWORD,
-    auth0_domain: process.env.REACT_APP_AUTH0_DOMAIN,
-    auth0_audience: process.env.REACT_APP_AUTH0_AUDIENCE,
-    auth0_scope: process.env.REACT_APP_AUTH0_SCOPE,
-    auth0_client_id: process.env.REACT_APP_AUTH0_CLIENTID,
-    auth0_client_secret: process.env.AUTH0_CLIENT_SECRET,
-    auth_token_name: process.env.REACT_APP_AUTH_TOKEN_NAME,
+//     // Auth0
+//     auth0_username: process.env.AUTH0_USERNAME,
+//     auth0_password: process.env.AUTH0_PASSWORD,
+//     auth0_domain: process.env.REACT_APP_AUTH0_DOMAIN,
+//     auth0_audience: process.env.REACT_APP_AUTH0_AUDIENCE,
+//     auth0_scope: process.env.REACT_APP_AUTH0_SCOPE,
+//     auth0_client_id: process.env.REACT_APP_AUTH0_CLIENTID,
+//     auth0_client_secret: process.env.AUTH0_CLIENT_SECRET,
+//     auth_token_name: process.env.REACT_APP_AUTH_TOKEN_NAME,
 
-    // Okta
-    okta_username: process.env.OKTA_USERNAME,
-    okta_password: process.env.OKTA_PASSWORD,
-    okta_domain: process.env.REACT_APP_OKTA_DOMAIN,
-    okta_client_id: process.env.REACT_APP_OKTA_CLIENTID,
+//     // Okta
+//     okta_username: process.env.OKTA_USERNAME,
+//     okta_password: process.env.OKTA_PASSWORD,
+//     okta_domain: process.env.REACT_APP_OKTA_DOMAIN,
+//     okta_client_id: process.env.REACT_APP_OKTA_CLIENTID,
 
-    // Amazon Cognito
-    cognito_username: process.env.AWS_COGNITO_USERNAME,
-    cognito_password: process.env.AWS_COGNITO_PASSWORD,
-    awsConfig: awsConfig.default,
+//     // Amazon Cognito
+//     cognito_username: process.env.AWS_COGNITO_USERNAME,
+//     cognito_password: process.env.AWS_COGNITO_PASSWORD,
+//     awsConfig: awsConfig.default,
 
-    // Google
-    googleRefreshToken: process.env.GOOGLE_REFRESH_TOKEN,
-    googleClientId: process.env.REACT_APP_GOOGLE_CLIENTID,
-    googleClientSecret: process.env.REACT_APP_GOOGLE_CLIENT_SECRET,
-  },
-  component: {
-    devServer,
-    specPattern: "src/**/*.cy.{js,jsx,ts,tsx}",
-    supportFile: "cypress/support/component.ts",
-  },
-  e2e: {
-    specPattern: "cypress/tests/**/*.spec.{js,jsx,ts,tsx}",
-    supportFile: "cypress/support/e2e.ts",
-    viewportHeight: 1000,
-    viewportWidth: 1280,
-    setupNodeEvents(on, config) {
-      const testDataApiEndpoint = `${config.env.apiUrl}/testData`;
+//     // Google
+//     googleRefreshToken: process.env.GOOGLE_REFRESH_TOKEN,
+//     googleClientId: process.env.REACT_APP_GOOGLE_CLIENTID,
+//     googleClientSecret: process.env.REACT_APP_GOOGLE_CLIENT_SECRET,
+//   },
+//   component: {
+//     devServer,
+//     specPattern: "src/**/*.cy.{js,jsx,ts,tsx}",
+//     supportFile: "cypress/support/component.ts",
+//   },
+//   e2e: {
+//     specPattern: "cypress/tests/**/*.spec.{js,jsx,ts,tsx}",
+//     supportFile: "cypress/support/e2e.ts",
+//     viewportHeight: 1000,
+//     viewportWidth: 1280,
+//     setupNodeEvents(on, config) {
+//       const testDataApiEndpoint = `${config.env.apiUrl}/testData`;
 
-      const queryDatabase = ({ entity, query }, callback) => {
-        const fetchData = async (attrs) => {
-          const { data } = await axios.get(`${testDataApiEndpoint}/${entity}`);
-          return callback(data, attrs);
-        };
+//       const queryDatabase = ({ entity, query }, callback) => {
+//         const fetchData = async (attrs) => {
+//           const { data } = await axios.get(`${testDataApiEndpoint}/${entity}`);
+//           return callback(data, attrs);
+//         };
 
-        return Array.isArray(query) ? Promise.map(query, fetchData) : fetchData(query);
-      };
+//         return Array.isArray(query) ? Promise.map(query, fetchData) : fetchData(query);
+//       };
 
-      on("task", {
-        percyHealthCheck,
-        async "db:seed"() {
-          // seed database with test data
-          const { data } = await axios.post(`${testDataApiEndpoint}/seed`);
-          return data;
-        },
+//       on("task", {
+//         percyHealthCheck,
+//         async "db:seed"() {
+//           // seed database with test data
+//           const { data } = await axios.post(`${testDataApiEndpoint}/seed`);
+//           return data;
+//         },
 
-        // fetch test data from a database (MySQL, PostgreSQL, etc...)
-        "filter:database"(queryPayload) {
-          return queryDatabase(queryPayload, (data, attrs) => _.filter(data.results, attrs));
-        },
-        "find:database"(queryPayload) {
-          return queryDatabase(queryPayload, (data, attrs) => _.find(data.results, attrs));
-        },
-      });
+//         // fetch test data from a database (MySQL, PostgreSQL, etc...)
+//         "filter:database"(queryPayload) {
+//           return queryDatabase(queryPayload, (data, attrs) => _.filter(data.results, attrs));
+//         },
+//         "find:database"(queryPayload) {
+//           return queryDatabase(queryPayload, (data, attrs) => _.find(data.results, attrs));
+//         },
+//       });
 
-      codeCoverageTask(on, config);
-      return config;
-    },
-  },
-};
+//       codeCoverageTask(on, config);
+//       return config;
+//     },
+//   },
+// };
